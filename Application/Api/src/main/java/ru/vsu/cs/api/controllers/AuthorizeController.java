ebf42e8ab4357a77b73ccc2c1006fe57d98be7ab
Mishka <Fiz-mat228@yandex.ru>
2023-05-07 20:07:26 +0300

    fixed AuthorizeController.java and deleted UserDetails

--- Application/Api/src/main/java/ru/vsu/cs/api/controllers/AuthorizeController.java
+++ Application/Api/src/main/java/ru/vsu/cs/api/controllers/AuthorizeController.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class AuthorizeController {
     private final UserService userService;
     private final SavedMessageService savedMessageService;
