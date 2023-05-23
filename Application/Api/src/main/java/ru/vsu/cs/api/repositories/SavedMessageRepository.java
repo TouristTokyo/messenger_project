9bf0e94680a8ce71c81cabc2706eea484e20340f
Mishka <Fiz-mat228@yandex.ru>
2023-05-23 22:17:57 +0300

    fixed delete operation for saved messages

--- Application/Api/src/main/java/ru/vsu/cs/api/repositories/SavedMessageRepository.java
+++ Application/Api/src/main/java/ru/vsu/cs/api/repositories/SavedMessageRepository.java
@@ -2,6 +2,7 @@ package ru.vsu.cs.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.vsu.cs.api.models.Message;
 import ru.vsu.cs.api.models.SavedMessage;
 import ru.vsu.cs.api.models.User;
 
@@ -11,4 +12,8 @@ import java.util.List;
 @Repository
 public interface SavedMessageRepository extends JpaRepository<SavedMessage, BigInteger> {
     List<SavedMessage> findByUser(User user);
+
+    void deleteAllByUser(User user);
+
+    void deleteByMessageAndUser(Message message, User user);
 }
