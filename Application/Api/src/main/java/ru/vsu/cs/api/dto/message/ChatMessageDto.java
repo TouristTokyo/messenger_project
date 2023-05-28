42ea8fdae1036d9b5b3dfc735e41bef7a6cc5d70
Mishka <Fiz-mat228@yandex.ru>
2023-05-28 22:32:39 +0300

    fixed dto

--- Application/Api/src/main/java/ru/vsu/cs/api/dto/message/ChatMessageDto.java
+++ Application/Api/src/main/java/ru/vsu/cs/api/dto/message/ChatMessageDto.java
@@ -3,6 +3,7 @@ package ru.vsu.cs.api.dto.message;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import ru.vsu.cs.api.dto.supporting.ChatSupportingDto;
+import ru.vsu.cs.api.dto.supporting.UserSupportingDto;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
@@ -14,4 +15,5 @@ public class ChatMessageDto {
     private ChatSupportingDto chat;
     private String data;
     private LocalDateTime date;
+    private UserSupportingDto sender;
 }
