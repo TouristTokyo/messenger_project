c930d31140ee9015501c2df2b277e2c02a4b35e7
Mishka <Fiz-mat228@yandex.ru>
2023-05-29 22:06:54 +0300

    added description for supporting dto

--- Application/Api/src/main/java/ru/vsu/cs/api/dto/supporting/RoleSupportingDto.java
+++ Application/Api/src/main/java/ru/vsu/cs/api/dto/supporting/RoleSupportingDto.java
@@ -1,8 +1,10 @@
 package ru.vsu.cs.api.dto.supporting;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Дополнительная информация о роли")
 public class RoleSupportingDto {
     private String name;
     private boolean isAdmin;
